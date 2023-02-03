0dcefa20fbf748d14e9ce50a87fedd1349b40061
sminth <emmanuelmalan225@gmail.com>
2023-02-03 15:31:39 +0000

    maj de l'interface et des fonctions du code

--- app/src/main/java/code/with/cal/nucleus/MainActivity.kt
+++ app/src/main/java/code/with/cal/nucleus/MainActivity.kt
@@ -1,17 +1,13 @@
 //package com.example.myapplication
 package code.with.cal.nucleus
-
 import android.annotation.SuppressLint
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-//import androidx.navigation.findNavController
-//import androidx.navigation.ui.AppBarConfiguration
-//import androidx.navigation.ui.navigateUp
-//import androidx.navigation.ui.setupActionBarWithNavController
-//import com.example.myapplication.databinding.ActivityMainBinding
+
 import android.os.Bundle
 import android.view.View
+import android.view.View.OnLongClickListener
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -37,6 +33,7 @@ class MainActivity : AppCompatActivity() {
 //        }
 //    }
 
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +41,15 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setTitle("Ma super Calco")
         result = findViewById(id.result)
 
+        result.setTextIsSelectable(true)
+        result.setOnLongClickListener(OnLongClickListener {
+            val clipboard = getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("Texte Copié", result.getText())
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(applicationContext, "texte bien copié !", Toast.LENGTH_SHORT)
+                .show()
+            true
+        })
         if (savedInstanceState != null) {
 
 
@@ -117,6 +123,7 @@ class MainActivity : AppCompatActivity() {
         resetButton.setOnClickListener { reset() }
 
     }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         var res = result.text.toString()
@@ -141,7 +148,7 @@ class MainActivity : AppCompatActivity() {
         print(currentNumber)
         print(currentOperator)
 //        print(result)
-        if (currentOperator.isNotEmpty()){
+        if (currentOperator!=""){
 //            val resultCalcul =calc()
 //            result.text = resultCalcul.toString()
 //            currentNumber = resultCalcul.toString()
@@ -157,11 +164,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
     private fun negate() {
+        print(currentNumber)
+        print(previousNumber)
         if (currentNumber.isNotEmpty()) {
-            currentNumber = currentNumber.toInt().times(-1).toString()
-            result.text = currentNumber
+            currentNumber = currentNumber.toDouble().times(-1).toString()
+            if (previousNumber.isNotEmpty()) {
+                result.text = previousNumber+currentOperator+ currentNumber
+
+            }
+            else{
+                result.text = currentNumber
+            }
         } else if (previousNumber.isNotEmpty()) {
-            previousNumber = previousNumber.toInt().times(-1).toString()
+            previousNumber = previousNumber.toDouble().times(-1).toString()
             result.text = previousNumber
         }
     }
@@ -172,18 +187,18 @@ class MainActivity : AppCompatActivity() {
             result.text = resultCalcul.toString()
             isEqualClicked = true
             currentNumber = resultCalcul.toString()
-//            previousNumber = ""
+            previousNumber = ""
             currentOperator = ""
         }
     }
-    private fun calc(): Int {
+    private fun calc(): Double {
         return when (currentOperator) {
-            "+" -> previousNumber.toInt() + currentNumber.toInt()
-            "-" -> previousNumber.toInt() - currentNumber.toInt()
-            "*" -> previousNumber.toInt() * currentNumber.toInt()
-            "/" -> previousNumber.toInt() / currentNumber.toInt()
-            "%" -> previousNumber.toInt() % currentNumber.toInt()
-            else -> 0
+            "+" -> previousNumber.toDouble() + currentNumber.toDouble()
+            "-" -> previousNumber.toDouble() - currentNumber.toDouble()
+            "*" -> previousNumber.toDouble() * currentNumber.toDouble()
+            "/" -> previousNumber.toDouble() / currentNumber.toDouble()
+            "%" -> previousNumber.toDouble() % currentNumber.toDouble()
+            else -> 0.0
         }
     }
 
@@ -194,11 +209,20 @@ class MainActivity : AppCompatActivity() {
             if (isOperatorClicked) {
                 currentOperator = ""
                 isOperatorClicked = false
+
             } else {
                 val lastChar = text[text.length - 1]
                 if(text[0] == '-' && text.length == 2 ){
                     reset() // On retire le signe "-" et le chiffre
                     return
+                }
+                else if(text.length > 2 && text[text.length - 2] == '-' && text[text.length - 1].isDigit()){
+                    result.text = text.substring(0, text.length - 2)
+                    currentNumber = currentNumber.substring(0, currentNumber.length - 1)
+                    isOperatorClicked = false
+                    currentOperator = ""
+
+
                 }
                 else {
                     if (currentNumber.length == 0) {
@@ -207,10 +231,10 @@ class MainActivity : AppCompatActivity() {
                     }
                     else  currentNumber = currentNumber.substring(0, currentNumber.length - 1)
                 }
-
             }
         }
     }
+
 
     private fun reset() {
         result.text = ""
